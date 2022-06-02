785c02b80604071331492728f9a03094209b5269
Milena <milenamoraesrc@gmail.com>
2022-06-01 22:47:43 -0300

    Atualizando aula-08

--- Bimestre 3/Front-End II/aula-05/mesadetrabalho5/main.js
+++ Bimestre 3/Front-End II/aula-05/mesadetrabalho5/main.js
@@ -1,70 +1,63 @@
-var novoPost = {
-    foto: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-    titulo: 'Gato',
-    texto: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, accusamus?'
+///// Dark Theme
+const bodyElement = document.querySelector('body')
+
+function replaceTheme() {
+  bodyElement.classList.toggle('dark-mode')
 }
+
 var containerElemento = document.querySelector('.container')
+var novaPostagemElemento = document.querySelector('#novaPostagem')
+
+
+
+// variaveis para armazenar os dados do input
+var inputFoto = document.querySelector("#foto");
+var inputTitulo = document.querySelector("#titulo");
+var inputTexto = document.querySelector("#texto");
 
 var posts = [
-    {
-        foto: 'https://s3.static.brasilescola.uol.com.br/be/2022/01/tigre.jpg',
-        titulo: 'O tigre',
-        texto: 'O tigre (Panthera tigris) é uma das espécies da subfamília Pantherinae (família Felidae) pertencente ao gênero Panthera. É encontrado de forma nativa apenas no continente asiático; é um predador carnívoro e é a maior espécie de felino do mundo junto com o leão.'
-    },
-    {
-        foto: './imagens/leon.jpg',
-        titulo: 'O leão',
-        texto: 'O leão (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
-    },
-
-    {
-        foto:'./imagens/leopardo.jpg',
-        titulo: 'O leopardo',
-        texto: 'O leopardo (Panthera pardus) é um mamífero carnívoro da família dos felinos. Como três dos outros felinos do gênero Panthera: o leão, o tigre e a onça-pintada, são caracterizados por uma modificação do osso  hióide que lhes permite rugir.É também conhecido como pantera marrom e, quando tem a pelagem completamente escura, como pantera negra (melanista).'
-    },
-
-    {
-        foto: './imagens/pantera-negra.jpg',
-        titulo: 'A pantera negra',
-        texto: 'A pantera negra é uma variação escura (melanismo) de várias espécies de felinos grandes, especialmente o leopardo (Panthera pardus) e a onça-pintada (Panthera onca).Mas deve-se ressaltar que não se trata de uma espécie nova, nem mesmo de uma subespécie, é simplesmente uma variação negra destes animais'
-    },
-
-    {
-        foto: './imagens/jaguar.jpg',
-        titulo: 'O jaguar',
-        texto: 'O jaguar, ou jaguarete (Panthera onca) é um carnívoro felídeo da sub-família Panthera e do gênero Panthera. É a única das cinco espécies existentes deste gênero encontradas nas Américas. Também é o maior felino das Américas e o terceiro maior do mundo, depois do tigre (Panthera tigris) e do leão (Panthera leo).'
-    },
-
-    {   foto: './imagens/chita.jpg',
-        titulo: 'O guepardo',
-        texto: ' O guepardo, ou chita (Acinonyx jubatus) é um membro atípico da família felina. É o único representante vivo do gênero Acinonyx. Ele caça graças à sua visão e grande velocidade. É o animal terrestre mais rápido do mundo, atingindo uma velocidade máxima de 115 km/h em corridas de até quinhentos metros.'
-    },
-    {
-        foto: 'https://s3.static.brasilescola.uol.com.br/be/2022/01/tigre.jpg',
-        titulo: 'O tigre',
-        texto: 'O tigre (Panthera tigris) é uma das espécies da subfamília Pantherinae (família Felidae) pertencente ao gênero Panthera. É encontrado de forma nativa apenas no continente asiático; é um predador carnívoro e é a maior espécie de felino do mundo junto com o leão.'
-    },
-    {
-        foto: './imagens/leon.jpg',
-        titulo: 'O leão',
-        texto: 'O leão (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
-    },
-
-    {
-        foto:'./imagens/leopardo.jpg',
-        titulo: 'O leopardo',
-        texto: 'O leopardo (Panthera pardus) é um mamífero carnívoro da família dos felinos. Como três dos outros felinos do gênero Panthera: o leão, o tigre e a onça-pintada, são caracterizados por uma modificação do osso  hióide que lhes permite rugir.É também conhecido como pantera marrom e, quando tem a pelagem completamente escura, como pantera negra (melanista).'
-    }
-] 
+  {
+    foto: 'https://s3.static.brasilescola.uol.com.br/be/2022/01/tigre.jpg',
+    titulo: 'O tigre',
+    texto:
+      'O tigre (Panthera tigris) é uma das espécies da subfamília Pantherinae (família Felidae) pertencente ao gênero Panthera. É encontrado de forma nativa apenas no continente asiático; é um predador carnívoro e é a maior espécie de felino do mundo junto com o leão.'
+  },
+  {
+    foto: 'https://vejasp.abril.com.br/wp-content/uploads/2021/12/20211202-Tigre-de-bengala9-foto-Paulo-Gil-2.jpg-1.jpg',
+    titulo: 'O tigre versão Shine',
+    texto:
+      'O tigre (Panthera tigris) é uma das espécies da subfamília Pantherinae (família Felidae) pertencente ao gênero Panthera. É encontrado de forma nativa apenas no continente asiático; é um predador carnívoro e é a maior espécie de felino do mundo junto com o leão.'
+  },
+  {
+    foto: 'https://img.r7.com/images/leao-branco-juba-moya-19012022005116744',
+    titulo: 'O leão',
+    texto:
+      'O leão (Panthera leo) é um mamífero carnívoro da família dos felinos é uma das cinco espécies do gênero gênero Panthera. Os leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África subsahariana (com exceção das regiões florestais e das regiões de selva da Bacia do Congo) e uma pequena área do noroeste da Índia.'
+  }
+]
 
 for (let post of posts) {
-
-    containerElemento.innerHTML += `
+  containerElemento.innerHTML += `
         <div class="item">
             <img src="${post.foto}">
             <h2>${post.titulo}</h2>
             <p>${post.texto}</p>
         </div>
     `
-
 }
+
+// Utilizando o event listenner para adicionar. E nessa caso, foi removido o onclick do botao lá no html
+
+novaPostagemElemento.addEventListener('click', () => {
+      containerElemento.innerHTML += ` 
+        <div class="item"> 
+            <img src="${inputFoto.value}"> 
+            <h2>${inputTitulo.value}</  h2> 
+            <p>${inputTexto.value}</p> 
+        </div> `
+
+        inputFoto.value = ''
+        inputTitulo.value = ''
+        inputTexto.value = ''
+
+})
